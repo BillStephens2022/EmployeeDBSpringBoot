583fc90630484b73ec7ead67b614edfabc09f94e
BillStephens2022 <two4onebill@yahoo.com>
2024-02-06 14:51:04 -0500

    updated EmployeeRepository and EmployeeServiceImpl so that employee data is returned from the database sorted by last name

--- src/main/java/com/billstephens/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/billstephens/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,8 +3,15 @@ package com.billstephens.springboot.thymeleafdemo.dao;
 import com.billstephens.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    // add a method to sort by last name
+    // note the name of the method is important as Spring Data JPA will parse the method name
+    // and automatically create a SQL query based off of it.
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
